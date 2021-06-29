6574144d931d976be34536498886b44dfaaa80fd
Amine <algusamine@gmail.com>
2021-06-29 10:24:37 -0400

    commit tree b615ea4e551f2cf07957c86b38b18c1aacf20b66

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function saturdayFun(avtivity = "roller-skate") {
+    return `This Saturday, I want to ${avtivity}!`;
+  };
+  saturdayFun("bathe my dog");
+
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork("work from home");
+
+function wrapAdjective(name="*a hard worker*") {
+    const part1 = "You are"
+      return function() {
+            return `${part1} ${name}!`;
+   }
+  }
+wrapAdjective("||a dedicated programmer||")();
